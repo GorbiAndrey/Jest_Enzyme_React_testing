796c610e608f41e53a90bb974bce39a0e5d9e375
GorbiAndrey <agorbi@ya.ru>
2022-07-29 01:25:21 +0300

    Создал проект и добавил функционал автотестов

---
+++ src/Button/button.spec.js
@@ -0,0 +1,13 @@
+import { shallow } from "enzyme";
+import React from "react";
+import Button from "./button";
+
+describe("Button component", () => {
+  it("should call onClick method", () => {
+    const mockCallBack = jest.fn();
+    const component = shallow(<Button onClick={mockCallBack} />);
+    expect(mockCallBack.mock.calls.length).toEqual(0);
+    component.find(".btn").simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
